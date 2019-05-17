2751ab7462a85e1d325819636b570fc5075aa25d
Ildar Azizov <azizov@mysmartflat.ru>
2019-05-17 11:45:53 +0500

    collection url fix

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -2,5 +2,9 @@
 
 Welcome to the generated API reference.
 @if($showPostmanCollectionButton)
+@if($outputPath === '.')
+[Get Postman Collection](collection.json)
+@else
 [Get Postman Collection]({{url($outputPath.'/collection.json')}})
+@endif
 @endif
